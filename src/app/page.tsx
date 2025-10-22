fb672f6150c2b6036a472bae062c1f4ee3ecce66
Muflih-uk <muhammedmuflih875@gmail.com>
2025-10-22 22:08:11 +0530

    feat: Home Page Animation Added in About Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ const HeroSection: React.FC = () => (
     <div className="max-w-7xl mx-auto">
       <div className="grid lg:grid-cols-2 gap-12 lg:gap-16 items-center">
         <div className="pl-[35px] space-y-6 md:space-y-2">
-          <h1 className="text-4xl md:text-5xl lg:text-[64px] font-bold text-black">
+          <p className="text-4xl md:text-5xl lg:text-[64px] font-alegreyaSans font-bold text-black">
             We build softwares that defines YOU
-          </h1>
-          <p className="text-base md:text-lg lg:text-[24px] max-w-xl">
+          </p>
+          <div className="text-base md:text-lg lg:text-[24px] max-w-xl">
             Running your business in itself is a hassle. Drape will manage the
             software-side for you. From web to mobile interfaces, we got you
             covered.
-          </p>
+          </div>
         </div>
         <div className="flex justify-center lg:justify-end">
           <DotLottieReact src="/hero-anim.lottie" loop autoplay />
