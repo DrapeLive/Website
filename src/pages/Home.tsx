f3e77e6e3fd619ec24fe79fc80e948d801ae01fb
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2025-10-22 19:15:00 +0530

    refactor: Removed unneccessary code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,8 @@
-import React from 'react';
-import { Header } from '../components/layout';
-import Footer from '../components/layout/Footer';
-import About from '../components/About';
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
-
-
-const PhoneMockup: React.FC = () => (
-  <div className="relative w-45 h-70 md:w-64 md:h-[500px] mr-[150px] lg:w-62 lg:h-[450px]">
-    <div className="absolute inset-0 bg-beige-light rounded-[2.5rem] md:rounded-[3rem] border-[6px] md:border-8 border-olive shadow-2xl">
-      <div className="absolute top-6 left-1/2 transform -translate-x-1/2 w-20 h-6 bg-olive rounded-full"></div>
-    </div>
-  </div>
-);
+import React from "react";
+import { Header } from "../components/layout";
+import Footer from "../components/layout/Footer";
+import About from "../components/About";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const HeroSection: React.FC = () => (
   <section className="min-h-screen pt-32 md:pt-40 pb-16 px-6 md:px-12">
@@ -22,15 +13,13 @@ const HeroSection: React.FC = () => (
             We build softwares that defines YOU
           </h1>
           <p className="text-base md:text-lg lg:text-[24px] max-w-xl">
-            Running your business in itself is a hassle. Drape will manage the software-side for you. From web to mobile interfaces, we got you covered.
+            Running your business in itself is a hassle. Drape will manage the
+            software-side for you. From web to mobile interfaces, we got you
+            covered.
           </p>
         </div>
         <div className="flex justify-center lg:justify-end">
-          <DotLottieReact
-            src="/hero-anim.lottie"
-            loop
-            autoplay
-          />
+          <DotLottieReact src="/hero-anim.lottie" loop autoplay />
         </div>
       </div>
     </div>
@@ -42,8 +31,8 @@ export const Home: React.FC = () => {
     <div className="min-h-screen bg-beige-light">
       <Header />
       <HeroSection />
-      <About/>
-      <Footer/>
+      <About />
+      <Footer />
     </div>
   );
 };
