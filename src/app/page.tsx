32308fea1d5973bf18042f62bc40aab7a4d2903a
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2025-10-27 16:40:31 +0530

    update: Updated lottie animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,52 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Header } from "../components/header";
 import Footer from "../components/footer";
 import About from "../components/about";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
-const HeroSection: React.FC = () => (
-  <section className="min-h-screen pt-32 md:pt-40 px-6 md:px-12">
-    <div className="max-w-7xl mx-auto">
-      <div className="grid lg:grid-cols-2 gap-12 lg:gap-16 items-center">
-        <div className="pl-[20px] space-y-6 md:space-y-2">
-          <p className="text-4xl md:text-5xl lg:text-[64px] font-alegreyaSans font-bold text-black">
-            We build softwares that defines YOU
-          </p>
-          <div className="text-base md:text-lg lg:text-[24px] max-w-xl">
-            Running your business in itself is a hassle. Drape will manage the
-            software-side for you. From web to mobile interfaces, we got you
-            covered.
+const HeroSection: React.FC = () => {
+  const [lottieAnimSrc, setLottieAnimSrc] = useState<string>(
+    "hero-anim-load.json",
+  );
+  const [isLooping, setIsLooping] = useState<boolean>(false);
+
+  const onComplete = () => {
+    if (lottieAnimSrc == "hero-anim-load.json") {
+      setLottieAnimSrc("hero-anim-loop.json");
+      setIsLooping(true);
+    }
+  };
+
+  return (
+    <section className="min-h-screen pt-32 md:pt-40 pb-16 px-6 md:px-12">
+      <div className="max-w-7xl mx-auto">
+        <div className="grid lg:grid-cols-2 gap-12 lg:gap-16 items-center">
+          <div className="pl-[35px] space-y-6 md:space-y-2">
+            <p className="text-4xl md:text-5xl lg:text-[64px] font-alegreyaSans font-bold text-black">
+              We build softwares that defines YOU
+            </p>
+            <div className="text-base md:text-lg lg:text-[24px] max-w-xl">
+              Running your business in itself is a hassle. Drape will manage the
+              software-side for you. From web to mobile interfaces, we got you
+              covered.
+            </div>
+          </div>
+          <div className="flex justify-center lg:justify-end">
+            <DotLottieReact
+              src={String(lottieAnimSrc) || "hero-anim-load.json"}
+              dotLottieRefCallback={(dotLottie) => {
+                dotLottie!.addEventListener("complete", onComplete);
+              }}
+              loop={isLooping}
+              autoplay
+            />
           </div>
         </div>
-        <div className="flex justify-center lg:justify-end">
-          <DotLottieReact src="/hero-anim.lottie" loop autoplay />
-        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 const Home: React.FC = () => {
   return (
